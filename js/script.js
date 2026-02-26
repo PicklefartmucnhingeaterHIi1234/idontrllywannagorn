493a9209d79b882549bbee9511b2d3ab6ca096f2
asdasdasda sdasd <165959957+PicklefartmucnhingeaterHIi1234@users.noreply.github.com>
2026-02-26 02:19:52 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const video = document.getElementById("player");
+    video.volume = 0.3;
+});
+
---
+++ js/titles.js
@@ -0,0 +1,4 @@
+const rrr=["smokedope2016","in da party","!!!"];let vvv=0;function yyy(){document.title=rrr[vvv];vvv=(vvv+1)%rrr.length}
+setInterval(yyy,500)
+
+
